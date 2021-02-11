174330906cbbdac63302937a821961d4ea7de32e
anaelleray <anaelleray7@gmail.com>
2021-02-11 10:44:40 +0100

    add grp name to deploy.yml

--- dist/repositories/user.repository.js
+++ dist/repositories/user.repository.js
@@ -8,7 +8,7 @@ class UserRepositoryService {
             /* DB.query(queryString)
                  .then((results: Array<any>) => {
                      resolve(results.map(row => new User(row)))
-                 }).catch(e => {
+                 }).catch(e =>  {
                      reject(e)
                  })*/
         });
